c2939d6b45395a7821447691edc38053306a4c35
Nick Nisi <nick@nisi.org>
2024-03-18 08:38:13 -0500

    setup tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
-import { Button } from '@material-ui/core';
+import { Button, Box } from '@material-ui/core';
 import './App.css';
 
 function App() {
 	return (
 		<>
-			<Button variant="contained" color="primary">
-				Hello World
-			</Button>
+			<Box m={4}>
+				<Button variant="contained" color="primary">
+					Hello World
+				</Button>
+			</Box>
+			<Box p={3}>
+				<div className="bg-red-400 text-blue-200">Hello Tailwind</div>
+			</Box>
 		</>
 	);
 }
